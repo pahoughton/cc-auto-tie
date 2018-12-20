26ea0abd84d209eaa4572a3f2cac19ca1ca60d91
Paul Houghton <paul4hough@gmail.com>
2018-12-20 16:06:51 -0700

    initial

---
+++ main.go
@@ -0,0 +1,31 @@
+/* 2018-12-20 (cc) <paul4hough@gmail.com>
+   copied from github.com/prometheus/alertmanager/examples/webhook/echo.go
+*/
+package main
+
+import (
+        "bytes"
+        "encoding/json"
+        "io/ioutil"
+        "log"
+        "net/http"
+)
+
+func main() {
+	log.Fatal(
+		http.ListenAndServe(
+			":5001",
+			http.HandlerFunc(
+	 			func(w http.ResponseWriter, r *http.Request) {
+					b, err := ioutil.ReadAll(r.Body)
+					if err != nil {
+                        panic(err)
+					}
+					defer r.Body.Close()
+					var buf bytes.Buffer
+					if err := json.Indent(&buf, b, " >", "  "); err != nil {
+                        panic(err)
+					}
+					log.Println(buf.String())
+				})))
+}
