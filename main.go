f73ab6727555bbd69ee71964ac47cf3c9b43cda0
Paul Houghton <paul4hough@gmail.com>
2018-12-20 18:23:44 -0700

    add arg and env support with kingpin

--- main.go
+++ main.go
@@ -6,9 +6,28 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	app = kingpin.New(filepath.Base(os.Args[0]),
+		"http dumper service")
+
+	listenAddr = app.Flag("laddr","listen address").
+		Short('l').
+		Default(":5001").
+		Envar("MAUL_LISTEN").
+		String()
+
 )
 
 func main() {
+	app.Version("0.0.1")
+	kingpin.MustParse(app.Parse(os.Args[1:]))
+
 	http.HandleFunc("/",alertHandler)
-	log.Fatal(http.ListenAndServe(":5001",nil))
+	log.Fatal(http.ListenAndServe(*listenAddr,nil))
 }
