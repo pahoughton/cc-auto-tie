f63642e7b6980238d59f1bf096c1434be8f4d93f
Paul Houghton <paul4hough@gmail.com>
2018-12-26 06:05:13 -0700

    improve debug output

--- mock-ticket/ticketHandler.go
+++ mock-ticket/ticketHandler.go
@@ -4,9 +4,12 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -23,6 +26,10 @@ func ticketHandler(
 		panic(err)
 	}
 	defer r.Body.Close()
+	var dbgbuf bytes.Buffer
+	if err := json.Indent(&dbgbuf, b, " >", "  "); err != nil {
+		log.Fatal(err)
+	}
 
 	resp := fmt.Sprintf(`<!DOCTYPE html>
 <html>
@@ -36,7 +43,8 @@ func ticketHandler(
 </html>
 `,b)
 	log.Info("ticket created")
-	log.Debug(resp)
+	log.Debug("ticket resp")
+	fmt.Fprintf(os.Stderr,"DEBUG req body\n%s\n",dbgbuf.String())
 	w.WriteHeader(200)
 	w.Write([]byte(resp))
 }
